2eddb055f1677b355465ecec1542b0ff28dffb2b
Nicolaas <nicolaas@Admins-iMac.local>
2018-12-13 22:29:12 +0200

    test

--- www/download_manager.js
+++ www/download_manager.js
@@ -1,17 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.download = function(arg0, success, error) {
-    exec(success, error, "DownloadManager", "download", [arg0]);
+    exec(success, error, "DownloadManager", "download", arg0);
 };
 
 exports.status = function(arg0, success, error) {
     exec(success, error, "DownloadManager", "status", [arg0]);
-};
-
-exports.storage_available = function(arg0, success, error) {
-    exec(success, error, "DownloadManager", "storage_available", [arg0]);
-};
-
-exports.foobar = function(arg0, success, error) {
-    exec(success, error, "DownloadManager", "foobar", [arg0]);
 };
