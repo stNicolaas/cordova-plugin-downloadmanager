031ac43b78c8cd228160be3d3558e470279b64f9
Nicolaas <nicolaas@Admins-iMac.local>
2018-12-13 22:34:30 +0200

    test

--- src/android/DownloadManager.java
+++ src/android/DownloadManager.java
@@ -24,8 +24,7 @@ public class DownloadManager extends CordovaPlugin {
       String title = args.getString(0);
       String download_url = args.getString(1);
       String destination_url = args.getString(2);
-      String filename = args.getString(3);
-      this.startDownload(title, download_url, destination_url, filename, callbackContext);
+      this.startDownload(title, download_url, destination_url, callbackContext);
       return true;
     }
     if (action.equals("status")) {
@@ -54,7 +53,7 @@ public class DownloadManager extends CordovaPlugin {
     }
   }
 
-  private void startDownload(String title, String download_url, String destination_url, String filename, CallbackContext callbackContext) {
+  private void startDownload(String title, String download_url, String destination_url, CallbackContext callbackContext) {
     try {
       if (download_url != null && download_url.length() > 0) {
         android.app.DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);
@@ -65,7 +64,7 @@ public class DownloadManager extends CordovaPlugin {
         request.setTitle(title);
         request.setDescription(title);
         request.setDestinationUri(uri);
-        request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), destination_url, filename);
+        request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), "", destination_url);
         request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); 
         // VISIBILITY_VISIBLE | VISIBILITY_HIDDEN | VISIBILITY_VISIBLE_NOTIFY_COMPLETED
         long downloadReference = downloadManager.enqueue(request);
