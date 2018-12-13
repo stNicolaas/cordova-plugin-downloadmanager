4c384fc7cc7c4f8239a8a2273b9a977a49ffc33e
Nicolaas <nicolaas@Admins-iMac.local>
2018-12-13 22:24:00 +0200

    test

--- src/android/DownloadManager.java
+++ src/android/DownloadManager.java
@@ -55,22 +55,26 @@ public class DownloadManager extends CordovaPlugin {
   }
 
   private void startDownload(String title, String download_url, String destination_url, String filename, CallbackContext callbackContext) {
-    if (download_url != null && download_url.length() > 0) {
-      android.app.DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);
-      Uri uri = Uri.parse(download_url);
-      android.app.DownloadManager.Request request = new android.app.DownloadManager.Request(uri);
-      request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI | android.app.DownloadManager.Request.NETWORK_MOBILE);
-      request.setAllowedOverRoaming(false);
-      request.setTitle(title);
-      request.setDescription(title);
-      request.setDestinationUri(uri);
-      request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), destination_url, filename);
-      request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); 
-      // VISIBILITY_VISIBLE | VISIBILITY_HIDDEN | VISIBILITY_VISIBLE_NOTIFY_COMPLETED
-      long downloadReference = downloadManager.enqueue(request);
-      callbackContext.success(Long.toString(downloadReference));
-    } else {
-      callbackContext.error("Expected one non-empty string argument.");
+    try {
+      if (download_url != null && download_url.length() > 0) {
+        android.app.DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(download_url);
+        android.app.DownloadManager.Request request = new android.app.DownloadManager.Request(uri);
+        request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI | android.app.DownloadManager.Request.NETWORK_MOBILE);
+        request.setAllowedOverRoaming(false);
+        request.setTitle(title);
+        request.setDescription(title);
+        request.setDestinationUri(uri);
+        request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), destination_url, filename);
+        request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); 
+        // VISIBILITY_VISIBLE | VISIBILITY_HIDDEN | VISIBILITY_VISIBLE_NOTIFY_COMPLETED
+        long downloadReference = downloadManager.enqueue(request);
+        callbackContext.success(Long.toString(downloadReference));
+      } else {
+        callbackContext.error("Expected one non-empty string argument.");
+      }
+    } catch (Exception e) {
+      callbackContext.error(e.getMessage());
     }
   }
   
